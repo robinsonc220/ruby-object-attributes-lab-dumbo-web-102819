9b2cf946f6b24ee44e2d0bc91681bd0d7f1c884e
Conrad Robinson <robinsonc220@gmail.com>
2019-10-29 19:49:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog 
 
+<<<<<<< HEAD
     def initialize
     
     end
@@ -20,3 +21,13 @@ class Dog
         @breed = breed
     end
 end   
+=======
+def name 
+  @name = name
+end
+
+def name=
+@name =
+
+end
+>>>>>>> 0b3dae328b175da9cd5b99dd9753c17f4622e600
